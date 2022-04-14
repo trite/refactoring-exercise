73b8eaa944422b9908337054cbbf033a577ff43a
trite <trite@users.noreply.github.com>
2022-04-13 20:03:47 -0700

    Test modifications

--- __tests__/Main_test.bs.js
+++ __tests__/Main_test.bs.js
@@ -5,30 +5,37 @@ var Jest = require("@glennsl/rescript-jest/src/jest.bs.js");
 var Main = require("../src/Main.bs.js");
 
 Jest.describe("Main", (function (param) {
-        var x = [
-          "asdf",
-          " ",
-          "asdf2"
-        ];
-        Main.doThingsAndStuff(x);
-        Jest.test("return 1 set of spaces in array", (function (param) {
-                return Jest.Expect.toEqual(Jest.Expect.expect(x), [" "]);
-              }));
-        var x$1 = [
-          "   ",
-          "asdf",
-          " ",
-          "asdf2",
-          "     "
-        ];
-        Main.doThingsAndStuff(x$1);
-        return Jest.test("multiple sets of spaces are returned in reverse order", (function (param) {
-                      return Jest.Expect.toEqual(Jest.Expect.expect(x$1), [
-                                  "     ",
-                                  " ",
-                                  "   "
-                                ]);
-                    }));
+        var testExpect = function (toSort, expectedResult, msg) {
+          Main.doThingsAndStuff(toSort);
+          return Jest.test(msg, (function (param) {
+                        return Jest.Expect.toEqual(Jest.Expect.expect(toSort), expectedResult);
+                      }));
+        };
+        testExpect([
+              "asdf",
+              " ",
+              "asdf2"
+            ], [" "], "single space string");
+        testExpect([
+              "   ",
+              "asdf",
+              " ",
+              "asdf2",
+              "     "
+            ], [
+              "     ",
+              " ",
+              "   "
+            ], "multiple spaces, order partially fixed");
+        return testExpect([
+                    "     ",
+                    " ",
+                    "   "
+                  ], [
+                    "     ",
+                    "   ",
+                    " "
+                  ], "multiple spaces, order partially fixed");
       }));
 
 /*  Not a pure module */
--- __tests__/Main_test.res
+++ __tests__/Main_test.res
@@ -4,18 +4,25 @@ describe("Main", () => {
     open Main;
     open Expect;
 
+    let testExpect = (toSort, expectedResult, msg) => {
+        doThingsAndStuff(toSort) |> ignore
+        test(msg, () => expect(toSort) -> toEqual(expectedResult))
+    }
 
-    let x = ["asdf", " ", "asdf2"]
-    doThingsAndStuff(x) |> ignore
+    // let x = ["asdf", " ", "asdf2"]
+    // doThingsAndStuff(x) |> ignore
+    // test("single space string", () => expect(x) -> toEqual([" "]));
 
-    test("return 1 set of spaces in array", () =>
-        expect(x) -> toEqual([" "]));
+    testExpect(["asdf", " ", "asdf2"], [" "], "single space string")
+
+    testExpect(["   ", "asdf", " ", "asdf2", "     "], ["     ", " ", "   "], "multiple spaces, order partially fixed")
+    testExpect(["     ", " ", "   "], ["     ", "   ", " "], "multiple spaces, order partially fixed")
 
 
-    let x = ["   ", "asdf", " ", "asdf2", "     "]
-    doThingsAndStuff(x) |> ignore
+    // let x = ["   ", "asdf", " ", "asdf2", "     "]
+    // doThingsAndStuff(x) |> ignore
 
-    test("multiple sets of spaces are returned in reverse order", () =>
-        expect(x) -> toEqual(["     ", " ", "   "]));
+    // test("multiple sets of spaces are returned in reverse order", () =>
+    //     expect(x) -> toEqual(["     ", " ", "   "]));
     
 });
